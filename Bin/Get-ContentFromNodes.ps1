db0269be3f2e3b2a20fbce7c82ca7a1024221b01
Martin Walther <it@martin-walther.ch>
2022-06-27 15:05:15 +0200

    Update some code and tests

--- Bin/Get-ContentFromNodes.ps1
+++ Bin/Get-ContentFromNodes.ps1
@@ -1,4 +1,6 @@
 #Requires –Modules psyml
+[CmdletBinding()]
+param ()
 
 #region Folders
 $RootFolder = $PSScriptRoot | Split-Path -Parent
@@ -7,12 +9,22 @@ $NodeFolder = Join-Path -Path $RootFolder -ChildPath 'Nodes'
 $TestFolder = Join-Path -Path $RootFolder -ChildPath 'Tests'
 #endregion
 
+Write-Verbose $($RootFolder)
+Write-Verbose $($NodeFolder)
+
 #region Nodes
 $NodeContent = foreach($node in (Get-ChildItem $NodeFolder)){
+    Write-Verbose $node.Name
     Get-Content -Path $node.FullName | ConvertFrom-Yaml
 }
 $NodeNew  = ($NodeContent).Where({$_.Status -match 'new'})
 $NodeDone = ($NodeContent).Where({$_.Status -match 'done'})
+
+Write-Verbose "Name: $($NodeContent.Name | Out-String)"
+Write-Verbose "Status: $($NodeContent.Status | Out-String)"
 #endregion
+
+Write-Verbose $($NodeNew.Name)
+Write-Verbose $($NodeDone.Name)
 
 return $NodeDone, $NodeNew
--- Tests/YamlSyntax.Tests.ps1
+++ Tests/YamlSyntax.Tests.ps1
@@ -11,11 +11,62 @@ BeforeDiscovery {
     #endregion
 }
 
+Describe "Check Yaml-Files" {
 
-Describe "Check Yaml-files" {
-
-    It "[POS] <Nodes.Name> should not throw" -ForEach @( @{ Nodes = $NodeFiles } ){
+    It "[POS] Reading of <Nodes.Name> should not throw" -ForEach @( @{ Nodes = $NodeFiles } ){
         { $Nodes.Fullname | ConvertFrom-Yaml } | Should -Not -Throw
+    }
+
+}
+
+Describe "Check Yaml-Content" {
+
+    BeforeDiscovery {
+        #region Folders
+        $RootFolder = $PSScriptRoot | Split-Path -Parent
+        $NodeFolder = Join-Path -Path $RootFolder -ChildPath 'Nodes'
+        #endregion
+    
+        #region Nodes
+        $NodeContent = foreach($node in (Get-ChildItem $NodeFolder)){
+            Write-Verbose $node.Name
+            Get-Content -Path $node.FullName | ConvertFrom-Yaml
+        }        
+        #endregion
+    }
+
+    It "[POS] Get YAML-Nodes <NodeContent.Name> should BeOfType PSCustomObject" -ForEach @( @{ NodeContent = $NodeContent } ){
+        ( $NodeContent ) | Should -BeOfType [PSCustomObject]
+    }
+    
+    It "[POS] Get YAML-Nodes Name should have a value" -TestCases @( @{ NodeContent = $NodeContent } ){
+        foreach($item in $NodeContent){
+            ($item.Name.length) | Should -BeGreaterThan 0
+        } 
+    }
+    It "[POS] Get YAML-Nodes IPAddress should have a value" -TestCases @( @{ NodeContent = $NodeContent } ){
+        foreach($item in $NodeContent){
+            ($item.IPAddress.length) | Should -BeGreaterThan 0
+        } 
+    }
+    It "[POS] Get YAML-Nodes Subnet should have a value" -TestCases @( @{ NodeContent = $NodeContent } ){
+        foreach($item in $NodeContent){
+            ($item.Subnet.length) | Should -BeGreaterThan 0
+        } 
+    }
+    It "[POS] Get YAML-Nodes Gateway should have a value" -TestCases @( @{ NodeContent = $NodeContent } ){
+        foreach($item in $NodeContent){
+            ($item.Gateway.length) | Should -BeGreaterThan 0
+        } 
+    }
+    It "[POS] Get YAML-Nodes Status should have a value" -TestCases @( @{ NodeContent = $NodeContent } ){
+        foreach($item in $NodeContent){
+            ($item.Status.length) | Should -BeGreaterThan 0
+        } 
+    }
+
+    It "[POS] Get YAML-Nodes <NodeContent.Name> should Contain Status 'new'" -ForEach @( @{ NodeContent = $NodeContent } ){
+        ( $NodeContent.Status ) | Should -Contain 'new'
     }
 
 }
