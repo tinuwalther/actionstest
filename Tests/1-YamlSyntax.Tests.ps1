b7e063564a291775e41bfdbd4161218a38ed66d8
Martin Walther <it@martin-walther.ch>
2022-07-03 15:50:46 +0200

    Change Describe with ForEach

---
+++ Tests/1-YamlSyntax.Tests.ps1
@@ -0,0 +1,15 @@
+#Requires –Modules psyml
+
+BeforeDiscovery {
+    $RootFolder = $PSScriptRoot | Split-Path -Parent
+    $NodeFolder = Join-Path -Path $RootFolder -ChildPath 'Nodes'
+    $NodeFiles  = Get-ChildItem $NodeFolder
+}
+
+Describe "Validate Yaml Syntax from <_.Name>" -Tag 'Required' -ForEach $NodeFiles {
+
+    It "[POS] Reading of <_.Name> should not throw" {
+        { $_.Fullname | ConvertFrom-Yaml } | Should -Not -Throw
+    }
+
+}
---
+++ Tests/2-InputValidation.Tests.ps1
@@ -0,0 +1,30 @@
+BeforeDiscovery {
+    $RootFolder = $PSScriptRoot | Split-Path -Parent
+    $NodeFolder = Join-Path -Path $RootFolder -ChildPath 'Nodes'
+    $yamlpath   = Join-Path -Path $NodeFolder -ChildPath '*.yml'
+    $yamlfile   = Get-ChildItem $yamlpath
+}
+
+Describe "Validate Input from <_.Name>" -Tag 'Required' -ForEach $yamlfile {
+
+    BeforeAll{
+        $yamlobject = get-content $_.FullName | ConvertFrom-Yaml
+    }
+
+    It "[POS] The Property Name should contains characters and numbers" {
+        $($yamlobject.Name) | Should -Match "\w+\d+"
+    }
+    It "[POS] The Property IPAddress should contains IPv4Address" {
+        $($yamlobject.IPAddress) | Should -Match '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+    }
+    It "[POS] The Property Subnet should contains Subnet mask" {
+        $($yamlobject.Subnet) | Should -Match '^(((255\.){3}(255|254|252|248|240|224|192|128+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$'
+    }
+    It "[POS] The Property Gateway should contains IPv4Address" {
+        $($yamlobject.Gateway) | Should -Match '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+    }
+    It "[POS] The Property Status should contains done or new" {
+        $($yamlobject.Status) | Should -Match 'done|new'
+    }
+
+}
