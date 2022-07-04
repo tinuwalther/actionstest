f73113ea19084f13f13bff04fef07a4aae88ee23
Martin Walther <it@martin-walther.ch>
2022-07-04 10:51:22 +0200

    Update workflow

--- Bin/Read-FromXML.ps1
+++ Bin/Read-FromXML.ps1
@@ -7,103 +7,101 @@
     $PesterNUnitXml = ConvertFrom-PesterNUnitXml -InputFile $InputFile
     $PesterNUnitXml
 #>
-function ConvertFrom-PesterNUnitXml{
-    [CmdletBinding()]
-    param (
-        [Parameter(
-            Mandatory = $true,
-            ValueFromPipeline = $true,
-            ValueFromPipelineByPropertyName = $true,
-            Position = 0
-        )]
-        [String]$InputFile
-    )
+[CmdletBinding()]
+param (
+    [Parameter(
+        Mandatory = $true,
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true,
+        Position = 0
+    )]
+    [String]$InputFile
+)
 
-    begin{
-        $function = $($MyInvocation.MyCommand.Name)
-        Write-Verbose "[Begin]   $function"
-    }
+begin{
+    $function = $($MyInvocation.MyCommand.Name)
+    Write-Verbose "[Begin]   $function"
+}
 
-    process {
-        Write-Verbose "[Process] $function"
+process {
+    Write-Verbose "[Process] $function"
 
-        if(Test-Path -Path $InputFile){
-            [xml]$doc = Get-Content -path $InputFile
-            if ($doc.'test-results'.noNamespaceSchemaLocation -match "nunit") {    
+    if(Test-Path -Path $InputFile){
+        [xml]$doc = Get-Content -path $InputFile
+        if ($doc.'test-results'.noNamespaceSchemaLocation -match "nunit") {    
 
-                $doc.'test-results' | ForEach-Object {
-                    $TestComputer      = $_.environment.'machine-name'
-                    $TotalCount        = ([int]$_.total) + [int]($_.'not-run')
-                    $PassedCount       = ([int]$_.total) - ([int]($_.failures) + [int]($_.skipped) + ([int]$_.errors) + ([int]$_.inconclusive) + ([int]$_.ignored) + ([int]$_.invalid))
-                    $ErrorCount        = [int]$_.errors
-                    $FailedCount       = [int]$_.failures
-                    $SkippedCount      = [int]$_.skipped
-                    $NotRunCount       = [int]$_.'not-run'
-                    $InconclusiveCount = [int]$_.inconclusive
-                    $IgnoredCount      = [int]$_.ignored
-                    $InvalidCount      = [int]$_.invalid
-                    $ExecutedAt        = Get-Date "$($_.date) $($_.time)" -f 'yyyy-MM-dd HH:mm:ss'
-                }
-
-                $doc.'test-results'.'test-suite' | ForEach-Object {
-                    $Result     = $_.result
-                    $Duration   = $_.time
-                }
-
-                $doc.'test-results'.'test-suite'.results.'test-suite' | ForEach-Object {
-                    $PesterFile     = $_.name
-                }
-
-                $Tests = $doc.'test-results'.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-case' | ForEach-Object {
-                    if($_.result -match "Failure"){
-                        $null = $_.failure.message -match '(?<=^)(.*)(?=\.)'
-                        $Message = $matches[0]
-                    }elseif($_.result -match "Success"){
-                        $Message = "Success"
-                    }else{
-                        $Message = $null
-                    }
-                    $TestName = $_.name -split '\.'
-                    [PSCustomObject]@{
-                        TestName    = $TestName[0]
-                        Description = $_.description
-                        Status      = $_.result
-                        Duration    = $_.time
-                        Message     = $Message
-                    }
-                }
-
-                $PesterResult = [PSCustomObject]@{
-                    PesterFile   = $PesterFile
-                    ExecutedAt   = $ExecutedAt
-                    TestComputer = $TestComputer
-
-                    TotalCount        = $TotalCount
-                    PassedCount       = ($Tests).Where({ $_.status -match "Success" }).Count
-                    FailedCount       = $FailedCount
-                    ErrorCount        = $ErrorCount
-                    SkippedCount      = $SkippedCount
-                    NotRunCount       = $NotRunCount
-                    InconclusiveCount = $InconclusiveCount
-                    IgnoredCount      = $IgnoredCount
-                    InvalidCount      = $InvalidCount
-
-                    Duration     = $Duration
-                    Result       = $Result
-
-                    Passed       = ($Tests).Where({ $_.status -match "Success" })  | Select-Object TestName, Description, Status, Message, Duration
-                    Failed       = ($Tests).Where({ $_.status -match "Failure" })  | Select-Object TestName, Description, Status, Message, Duration
-                    Skipped      = ($Tests).Where({ $_.status -match "Skipped" }) | Select-Object TestName, Description, Status, Message, Duration
-                    NotRun       = ($Tests).Where({ $_.status -match "NotRun" })  | Select-Object TestName, Description, Status, Message, Duration
-
-                    Tests        = $Tests
-                } 
+            $doc.'test-results' | ForEach-Object {
+                $TestComputer      = $_.environment.'machine-name'
+                $TotalCount        = ([int]$_.total) + [int]($_.'not-run')
+                $PassedCount       = ([int]$_.total) - ([int]($_.failures) + [int]($_.skipped) + ([int]$_.errors) + ([int]$_.inconclusive) + ([int]$_.ignored) + ([int]$_.invalid))
+                $ErrorCount        = [int]$_.errors
+                $FailedCount       = [int]$_.failures
+                $SkippedCount      = [int]$_.skipped
+                $NotRunCount       = [int]$_.'not-run'
+                $InconclusiveCount = [int]$_.inconclusive
+                $IgnoredCount      = [int]$_.ignored
+                $InvalidCount      = [int]$_.invalid
+                $ExecutedAt        = Get-Date "$($_.date) $($_.time)" -f 'yyyy-MM-dd HH:mm:ss'
             }
+
+            $doc.'test-results'.'test-suite' | ForEach-Object {
+                $Result     = $_.result
+                $Duration   = $_.time
+            }
+
+            $doc.'test-results'.'test-suite'.results.'test-suite' | ForEach-Object {
+                $PesterFile     = $_.name
+            }
+
+            $Tests = $doc.'test-results'.'test-suite'.results.'test-suite'.results.'test-suite'.results.'test-case' | ForEach-Object {
+                if($_.result -match "Failure"){
+                    $null = $_.failure.message -match '(?<=^)(.*)(?=\.)'
+                    $Message = $matches[0]
+                }elseif($_.result -match "Success"){
+                    $Message = "Success"
+                }else{
+                    $Message = $null
+                }
+                $TestName = $_.name -split '\.'
+                [PSCustomObject]@{
+                    TestName    = $TestName[0]
+                    Description = $_.description
+                    Status      = $_.result
+                    Duration    = $_.time
+                    Message     = $Message
+                }
+            }
+
+            $PesterResult = [PSCustomObject]@{
+                PesterFile   = $PesterFile
+                ExecutedAt   = $ExecutedAt
+                TestComputer = $TestComputer
+
+                TotalCount        = $TotalCount
+                PassedCount       = ($Tests).Where({ $_.status -match "Success" }).Count
+                FailedCount       = $FailedCount
+                ErrorCount        = $ErrorCount
+                SkippedCount      = $SkippedCount
+                NotRunCount       = $NotRunCount
+                InconclusiveCount = $InconclusiveCount
+                IgnoredCount      = $IgnoredCount
+                InvalidCount      = $InvalidCount
+
+                Duration     = $Duration
+                Result       = $Result
+
+                Passed       = ($Tests).Where({ $_.status -match "Success" })  | Select-Object TestName, Description, Status, Message, Duration
+                Failed       = ($Tests).Where({ $_.status -match "Failure" })  | Select-Object TestName, Description, Status, Message, Duration
+                Skipped      = ($Tests).Where({ $_.status -match "Skipped" }) | Select-Object TestName, Description, Status, Message, Duration
+                NotRun       = ($Tests).Where({ $_.status -match "NotRun" })  | Select-Object TestName, Description, Status, Message, Duration
+
+                Tests        = $Tests
+            } 
         }
     }
+}
 
-    end {
-        Write-Verbose "[End]     $function"
-        return $PesterResult
-    }
+end {
+    Write-Verbose "[End]     $function"
+    return $PesterResult
 }
