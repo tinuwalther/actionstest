1e21da3ca9574a9213199fe6f30a5c7e51019a27
Martin Walther <it@martin-walther.ch>
2022-07-04 21:12:47 +0200

    Update Pester Test

--- Tests/3-DuplicatedValues.Tests.ps1
+++ Tests/3-DuplicatedValues.Tests.ps1
@@ -1,43 +1,43 @@
-﻿BeforeDiscovery {
-    $RootFolder = $PSScriptRoot | Split-Path -Parent
-    $BinFolder  = Join-Path -Path $RootFolder -ChildPath 'Bin'
-    $NodeFolder = Join-Path -Path $RootFolder -ChildPath 'Nodes'
+﻿Describe "Test for duplicated values" -Tag 'Test' {
 
-    function Get-DuplicatedValue {
-        [CmdletBinding()]
-        param(
-            [Parameter(
-                Mandatory=$true,
-                ValueFromPipeline=$true,
-                ValueFromPipelineByPropertyName=$true,
-                Position = 0
-            )]
-            [String]$Filed
-        )
+    BeforeDiscovery {
+        $RootFolder = $PSScriptRoot | Split-Path -Parent
+        $BinFolder  = Join-Path -Path $RootFolder -ChildPath 'Bin'
+        $NodeFolder = Join-Path -Path $RootFolder -ChildPath 'Nodes'
+
+        function Get-DuplicatedValue {
+            [CmdletBinding()]
+            param(
+                [Parameter(
+                    Mandatory=$true,
+                    ValueFromPipeline=$true,
+                    ValueFromPipelineByPropertyName=$true,
+                    Position = 0
+                )]
+                [String]$Filed
+            )
     
-        # Duplicated fields
-        #Import-Module "$($env:ProgramFiles)\PowerShell\Modules\psyml"
-        $AllNodes = foreach($node in (Get-ChildItem $NodeFolder )){
-            $Content = Get-Content -Path $node.FullName | ConvertFrom-Yaml
-            $Content | Add-Member -Type NoteProperty -Name File -Value $node.Name
-            $Content
+            # Duplicated fields
+            #Import-Module "$($env:ProgramFiles)\PowerShell\Modules\psyml"
+            $AllNodes = foreach($node in (Get-ChildItem $NodeFolder )){
+                $Content = Get-Content -Path $node.FullName | ConvertFrom-Yaml
+                $Content | Add-Member -Type NoteProperty -Name File -Value $node.Name
+                $Content
+            }
+    
+            $Unique = $AllNodes | Select $Filed -unique
+    
+            $Properties = @{
+                ReferenceObject  = $AllNodes.$Filed
+                DifferenceObject = $Unique.$Filed
+            }
+            $Duplicated = Compare-Object @Properties -PassThru | Select -unique
+    
+            return $Duplicated
+    
         }
-    
-        $Unique = $AllNodes | Select $Filed -unique
-    
-        $Properties = @{
-            ReferenceObject  = $AllNodes.$Filed
-            DifferenceObject = $Unique.$Filed
-        }
-        $Duplicated = Compare-Object @Properties -PassThru | Select -unique
-    
-        return $Duplicated
     
     }
-    
-}
-
-Describe "Test for duplicated values" -Tag 'Required' {
 
     It "[POS] <_> should not have duplicated values" -ForEach 'Name', 'IPAddress' {
         Get-DuplicatedValue -Filed $_ | Should -BeNullOrEmpty
