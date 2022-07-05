3330b2aaec1f0ab0324a7c1a7d8d5388cd86487d
Martin Walther <it@martin-walther.ch>
2022-07-05 14:54:34 +0200

    Update 1-YamlSyntax.Tests.ps1

--- Tests/1-YamlSyntax.Tests.ps1
+++ Tests/1-YamlSyntax.Tests.ps1
@@ -6,7 +6,7 @@ BeforeDiscovery {
     $NodeFiles  = Get-ChildItem $NodeFolder
 }
 
-Describe "Validate Yaml Syntax from <_.Name>" -Tag 'Required' -ForEach $NodeFiles {
+Describe "Validate Yaml Syntax from <_.Name>" -Tag 'Test' -ForEach $NodeFiles {
 
     It "[POS] Reading of <_.Name> should not throw" {
         { $_.Fullname | ConvertFrom-Yaml } | Should -Not -Throw
